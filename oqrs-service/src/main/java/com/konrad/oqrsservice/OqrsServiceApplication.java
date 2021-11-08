da162239735e9ad29d8d69e63d5dc55c248e17ef
Konrad Barszczyk <kbarszczyk@protonmail.com>
2021-11-09 00:29:58 +0100

    initial commit

---
+++ oqrs-service/src/main/java/com/konrad/oqrsservice/OqrsServiceApplication.java
@@ -0,0 +1,13 @@
+package com.konrad.oqrsservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OqrsServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OqrsServiceApplication.class, args);
+    }
+
+}
---
+++ oqrs-service/src/test/java/com/konrad/oqrsservice/OqrsServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.konrad.oqrsservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OqrsServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
